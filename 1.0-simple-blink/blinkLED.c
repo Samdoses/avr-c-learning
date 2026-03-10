a4c0f0862f93ded05f44b3ce26f12996f20ff2b1
Samuel D'souza <samuel_dsouza@outlook.com>
2026-03-10 18:29:29 +0000

    implemened define macros for project 1.0

--- 1.0-simple-blink/blinkLED.c
+++ 1.0-simple-blink/blinkLED.c
@@ -2,24 +2,27 @@
 
 // ------- Preamble -------- //
 #include <avr/io.h>                        /* Defines pins, ports, etc */
-#include <util/delay.h>                     /* Functions to waste time */
+#include <util/delay.h>                    /* Functions to waste time */
 
+#define DELAYTIME 1000                     /* C macros to make code more readable and portable */
+#define LED_PORT  PORTB
+#define LED_DDR   DDRB
 
 int main(void) {
 
   // -------- Inits --------- //
-  DDRB |= (1 << 5);            /* Data Direction Register B:
+  LED_DDR |= (1 << 5);            /* Data Direction Register B:
                                    writing a one to the bit
                                    enables output. */
 
   // ------ Event loop ------ //
   while (1) {
 
-    PORTB |= (1 << 5);   /*turn on PB5 pin*/
-    _delay_ms(1000);    /*wait 1 s*/
+    LED_PORT |= (1 << 5);   /*turn on PB5 pin*/
+    _delay_ms(DELAYTIME);   /*wait 1 s*/
 
-    PORTB &= ~(1 << 5);   /*turn off PB5 pin*/
-    _delay_ms(1000);    /*wait 1 s*/                                              /* End event loop */
+    LED_PORT &= ~(1 << 5);   /*turn off PB5 pin*/
+    _delay_ms(DELAYTIME);    /*wait 1 s*/                                              /* End event loop */
   }
   return 0;                            /* This line is never reached */
 }
