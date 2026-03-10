8dcc33576df48a20feca36603152c2a2947161ea
Samuel D'souza <samuel_dsouza@outlook.com>
2026-03-10 18:51:11 +0000

    implemented define macros in project 1.1

--- 1.1-sos-beacon/blinkLED.c
+++ 1.1-sos-beacon/blinkLED.c
@@ -4,13 +4,17 @@
 #include <avr/io.h>                        /* Defines pins, ports, etc */
 #include <util/delay.h>                     /* Functions to waste time */
 
+#define LED_PORT PORTD
+#define LED_DDR  DDRD
 #define ALL_LEDS ((1 << 2) | (1 << 3) | (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7))
-
+#define SHORT_DELAYTIME 300
+#define LONG_DELAYTIME 900
+#define RESET_DELAYTIME 2000
 
 int main(void) {
 
   // -------- Inits --------- //
-  DDRD |= ALL_LEDS;            /* Data Direction Register D:
+  LED_DDR |= ALL_LEDS;            /* Data Direction Register D:
                                    writing a one to the bit
                                    enables output. */
 
@@ -19,22 +23,22 @@ int main(void) {
 
     for(int i = 0; i < 9; i++){
       if(i < 3 || i >= 6){
-        PORTD |= ALL_LEDS;   /*turn on PD2 -> PD7 pin*/
-        _delay_ms(300);      /*wait 1 s*/
+        LED_PORT |= ALL_LEDS;   /*turn on PD2 -> PD7 pin*/
+        _delay_ms(SHORT_DELAYTIME);      /*wait 300 ms*/
 
-        PORTD &= ~ALL_LEDS;   /*turn off PD2 -> PD7 pin*/
-        _delay_ms(300);      /*wait 1 s*/
+        LED_PORT &= ~ALL_LEDS;   /*turn off PD2 -> PD7 pin*/
+        _delay_ms(SHORT_DELAYTIME);      /*wait 300 ms*/
       }
 
       else{
-        PORTD |= ALL_LEDS;   /*turn on PD2 -> PD7 pin*/
-        _delay_ms(900);      /*wait 0.3 s*/
+        LED_PORT |= ALL_LEDS;   /*turn on PD2 -> PD7 pin*/
+        _delay_ms(LONG_DELAYTIME);      /*wait 900 ms*/
 
-        PORTD &= ~ALL_LEDS;   /*turn off PD2 -> PD7 pin*/
-        _delay_ms(300);      /*wait 0.3 s*/
+        LED_PORT &= ~ALL_LEDS;   /*turn off PD2 -> PD7 pin*/
+        _delay_ms(SHORT_DELAYTIME);      /*wait 300 ms*/
       }
     }
-    _delay_ms(2000);
+    _delay_ms(RESET_DELAYTIME);          /*wait 2000 ms, before resetting loop*/
   }                                                  /* End event loop */
   return 0;                            /* This line is never reached */
 }
