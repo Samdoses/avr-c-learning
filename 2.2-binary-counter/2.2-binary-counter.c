f8e89b647640b73d89483a4858d4fffe10a4ba06
Samdoses <samuel_dsouza@outlook.com>
2026-03-17 19:12:48 +0000

    implemented an oscilating 6-bit binary counter

---
+++ 2.2-binary-counter/2.2-binary-counter.c
@@ -0,0 +1,39 @@
+/*
+ *
+ * Cylon eyes actually turns out to be a decent POV routine.
+ * The only change here is in DELAYTIME.
+ *
+ */
+
+
+#include <avr/io.h>                        /* Defines pins, ports, etc */
+#include <util/delay.h>                     /* Functions to waste time */
+
+#define LED_PORT PORTD
+#define LED_DDR  DDRD
+#define ALL_LEDS ((1 << 2) | (1 << 3) | (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7))
+#define DELAYTIME 400                                    /* milliseconds */
+
+int main(void) {
+  uint8_t i=0;
+  LED_DDR = ALL_LEDS;              /* Data Direction Register B: all on */
+
+  while (1) {
+
+    while (i < 63) {
+      LED_PORT |= (i << 2);                 /* illuminate only i'th pin */
+      _delay_ms(DELAYTIME);                                     /* wait */
+      LED_PORT &= ~(i << 2);                    /* darken only i'th pin */
+      i = i + 1;                                /* move to the next LED */
+    }
+
+    while (i > 0) {
+      LED_PORT |= (i << 2);                /* illuminate only i'th pin */
+      _delay_ms(DELAYTIME);                                    /* wait */
+      LED_PORT &= ~(i << 2);                   /* darken only i'th pin */
+      i = i - 1;                           /* move to the previous LED */
+    }
+  }
+
+  return 0;
+}
